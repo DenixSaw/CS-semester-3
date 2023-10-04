dbf91f4cf7d9bae84ad760da4073fd471f3e4c4c
Calorific <kknd3390@mail.ru>
2023-10-04 21:16:49 +0700

    test(partition): test for systemType change
    test 1: setting memory to zero when changing system type test

---
+++ CS-semester-3.Tests/PartitionTests.cs
@@ -0,0 +1,16 @@
+﻿namespace CS_semester_3.Tests; 
+
+public class PartitionTests {
+    [Fact]
+    public void Should_Set_Memory_To_Zero_When_Changing_System_Type() {
+        // Arrange
+        var partition = new Partition("", 0, 1000, SystemType.FAT, 250);
+        ulong expectedMemoryUsed = 0;
+        
+        // Act
+        partition.SystemType = SystemType.ext4;
+        
+        // Assert
+        Assert.Equal(partition.MemoryUsed, expectedMemoryUsed);
+    }
+}
