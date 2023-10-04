83dd1c17422bea02509f12d53bcd7e17c3afb1e1
Calorific <kknd3390@mail.ru>
2023-10-04 22:14:42 +0700

    feat: basic calculator

---
+++ CS-semester-3/Calculator.cs
@@ -0,0 +1,19 @@
+﻿namespace CS_semester_3; 
+
+public class Calculator {
+    public static void Add(double x, double y) {
+        Console.WriteLine($"{x} + {y} = {x + y}");
+    }
+
+    public static void Multiply(double x, double y) {
+        Console.WriteLine($"{x} * {y} = {x * y}");
+    }
+
+    public static void Divide(double x, double y) {
+        Console.WriteLine($"{x} / {y} = {x / y}");
+    }
+
+    public static void Substract(double x, double y) {
+        Console.WriteLine($"{x} - {y} = {x - y}");
+    }
+}
--- CS-semester-3/Program.cs
+++ CS-semester-3/Program.cs
@@ -1,7 +1,60 @@
-﻿namespace CS_semester_3 {
-    internal class Program {
-        private static void Main() {
+﻿namespace CS_semester_3;
+
+internal class Program {
+    private static void Main() {
+        var operations = new[] { "+", "-", "*", "/" };
+        Console.BackgroundColor = ConsoleColor.Gray;
+        Console.ForegroundColor = ConsoleColor.Magenta;
+        Console.Clear();
+
+        while (true) {
+            Console.WriteLine("Введите операцию(+,-,*,/, 0):");
+            var key = Console.ReadKey();
+            if (key.Key == ConsoleKey.Escape)
+                break;
+            var op = key.KeyChar.ToString();
+
+            if (op == "0")
+                break;
+
+            if (!operations.Contains(op))
+                continue;
             
+            double a, b;
+            string input;
+            
+            do {
+                Console.WriteLine("\nВведите первое число:");
+                input = Console.ReadLine() ?? string.Empty;
+            } while (!double.TryParse(input, out a));
+
+            do {
+                Console.WriteLine("Введите второе число:");
+                input = Console.ReadLine() ?? string.Empty;
+            } while (!double.TryParse(input, out b));
+
+            switch (op) {
+                case "+":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Calculator.Add(a, b);
+                    break;
+                case "-":
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Calculator.Substract(a, b);
+                    break;
+                case "*":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Calculator.Multiply(a, b);
+                    break;
+                case "/":
+                    Console.BackgroundColor = ConsoleColor.Blue;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Calculator.Divide(a, b);
+                    break;
+            }
         }
     }
 }
