9d73f23cbd58203f5581c0dcc03491f86e9df9fe
Calorific <kknd3390@mail.ru>
2024-02-12 20:08:08 +0700

    feat(VirtualMemory): get and set element methods

--- CS-semester-3/Page.cs
+++ CS-semester-3/Page.cs
@@ -1,13 +1,16 @@
 namespace CS_semester_3; 
 
-public struct Page {
-    public int Index { get; }
+public class Page {
+    public long Index { get; }
     public bool IsModified { get; set; } = false;
     public DateTime UpdatedAt { get; set; } = DateTime.Now;
-    public bool[] BitMap { get; }
+    public byte[] BitMap { get; }
     public  int[] Data { get; }
 
-    public Page(int index, bool[] bitMap, int[] data) {
+    public Page(long index, byte[] bitMap, int[] data) {
+        if (index < 0)
+            throw new ArgumentException("Индекс не может быть отрицательным");
+        
         Index = index;
         BitMap = bitMap;
         Data = data;
--- CS-semester-3/VirtualMemory.cs
+++ CS-semester-3/VirtualMemory.cs
@@ -3,15 +3,30 @@ namespace CS_semester_3;
 public class VirtualMemory : IDisposable, IAsyncDisposable {
     private const int Offset = 2;
     private const int BufferSize = 4;
-    private const int PageSize = 512 + 128;
+    private const int BitMapSize = 128;
+    private const int DataSize = BitMapSize * sizeof(int);
+    private const int PageSize = BitMapSize + DataSize;
     private readonly Stream _fileStream;
     
     public string Filename { get; set; }
     public Page[] Buffer { get; }
+    
+    public long Length { get; }
     public long Size { get; }
+    
+    private void SavePage(Page page) {
+        _fileStream.Position = page.Index * PageSize + Offset - 1;
 
-    private int GetPage(int index) {
-        if (index > Size || index < 0)
+        using var binaryWriter = new BinaryWriter(_fileStream);
+        foreach (var value in page.BitMap)
+            binaryWriter.Write(value);
+        
+        foreach (var value in page.Data) 
+            binaryWriter.Write(value);
+    }
+    
+    private long GetPage(long index) {
+        if (index >= Length || index < 0)
             throw new IndexOutOfRangeException("Некорректный индекс");
         
         var pageIndex = index / PageSize;
@@ -20,51 +35,89 @@ public class VirtualMemory : IDisposable, IAsyncDisposable {
             return pageIndex;
         }
 
-        //Эту строчку убрать
-        return pageIndex;
-        //TODO: Сделать получение самой старой страницы
-        //var oldestPageIndex = Buffer.Aggregate()
+        var oldestPage = Buffer[0];
+        var oldestPageIndex = 0;
+
+        for (var i = 1; i < BufferSize; i++) {
+            if (Buffer[i].UpdatedAt >= oldestPage.UpdatedAt) continue;
+            oldestPage = Buffer[i];
+            oldestPageIndex = i;
+        }
+
+        if (oldestPage.IsModified)
+            SavePage(oldestPage);
+            
+        var bitMap = new byte[BitMapSize];
+
+        const int dataLength = DataSize / sizeof(int);
+        var data = new int[dataLength];
+        
+        _fileStream.Position = index * PageSize + Offset - 1;
+        using var binaryReader = new BinaryReader(_fileStream);
+        
+        for (var i = 0; i < BitMapSize; i++) 
+            bitMap[i] = binaryReader.ReadByte();
+        
+        for (var i = 0; i < dataLength; i++) 
+            data[i] = binaryReader.ReadInt32();
+
+        Buffer[oldestPageIndex] = new Page(pageIndex, bitMap, data);
+        return oldestPageIndex;
     }
     
     public bool GetElement(int index, out int result) {
+        var idx = GetPage(index);
 
-        result = 4;
+        if (Buffer[idx].BitMap[index % BitMapSize] != 1)
+            throw new NullReferenceException("Элемент не существует");
+        
+        result = Buffer[idx].Data[index % BitMapSize];
         return true;
     }
     
-    public VirtualMemory(long size, string filename = "./data.bin") {
+    public bool SetElement(int index, int item) {
+        var idx = GetPage(index);
+        Buffer[idx].BitMap[index % BitMapSize] = 1;
+        Buffer[idx].Data[index % BitMapSize] = item;
+        return true;
+    }
+    
+    public VirtualMemory(long length, string filename = "./data.bin") {
         Filename = filename;
-        Size = size;
+        Length = length;
+        Size = length * sizeof(int) + Offset;
         
-        if (File.Exists(filename)) {
-            _fileStream = File.Open(filename, FileMode.Create, FileAccess.ReadWrite);
-        }
-        else {
-            _fileStream = File.Open(filename, FileMode.Create, FileAccess.ReadWrite);
-
+        _fileStream = File.Open(filename, FileMode.Create, FileAccess.ReadWrite);
+        
+        if (!File.Exists(filename)) {
             using StreamWriter writer = new(_fileStream);
         
             writer.Write('V');
             writer.Write('M');
 
-            size -= (size - Offset) % PageSize + Offset;
-            for (long i = 0; i < size; i++)
+            Size += PageSize - Size % PageSize;
+            for (long i = 0; i < Size; i++)
                 writer.Write('\0');
         }
 
         Buffer = new Page[BufferSize];
 
-        // for (var i = 0; i < 3; i++) {
-        //     
-        // }
+        for (var i = 0; i < BufferSize; i++) 
+            GetElement(i * PageSize, out _);
     }
 
     public void Dispose() {
+        foreach (var page in Buffer) 
+            SavePage(page);
+        
         _fileStream.Dispose();
         GC.SuppressFinalize(this);
     }
 
     public async ValueTask DisposeAsync() {
+        foreach (var page in Buffer) 
+            SavePage(page);
+
         await _fileStream.DisposeAsync();
         GC.SuppressFinalize(this);
     }
